52a7d5a8806feed453d568bde7d1a9a91296fa11
Abdulbari Qaisar <abdulbari.112917@gmail.com>
2022-10-23 14:42:22 +0300

    Parsing the story

--- madlibs.js
+++ madlibs.js
@@ -28,7 +28,89 @@
  */
 function parseStory(rawStory) {
   // Your code here.
+  //creating empty array, then we will push the data to it
+  const results = [];
+
+  //change n , v , a to noun verb adjective
+  const positions ={
+    n:"noun" ,
+    v:"verb",
+    a:"adjective"
+  }
+
+
+  const words = rawStory.split(" ");
+words.map(word=>{
+
+  //check the word has positons or not, it's like (went[v]) or like (to ,the , and ... )
+if(word.includes("[")){
+// true, the word has position then
+
+// get the word only without the [v] [a] [n], output=> Louis , went , not look like louis[n] went[v]
+let getTheWord =word.substring(0,word.indexOf("["));
+
+//get the positions inside the [], output =>( [n] => n ), a , v ... , and change them to noun, adj , adverb by positons [n or a or v]
+let getThePositions = positions[word.substring(word.indexOf("[")+1 , word.indexOf("]"))];
+
+//push the word and with it's positions to the empty array
+results.push({word:getTheWord , pos:getThePositions})
+// if the word include dot or comma and at the same time include the [] like =>(fun[a].) , (store[n],)
+if(word.includes(',')){
+  results.push({word: ','})
+}
+else if(word.includes('.')){
+results.push({word: '.'})
+
+} 
+
+} 
+
+else {
+  // if the word has no positions or not include "[]", like (to, for, and , then ...)
+
+//if the word does not include dot and comma  like (to, for, and , then ...)
+if(!word.includes(".")&& !word.includes(",")){
+results.push({word:word})
+}
+
+// if the word include dot like (guess.)
+else if(word.includes(".")){
+
+  // get the word only without the dot like ( guess. => guess)
+  let getTheWordWithoutTheDot= word.substring(0 , word.indexOf("."))
+
+  //push the word 
+  results.push({word:getTheWordWithoutTheDot})
+
+
+  // get the dot without the the word like (guess. => .)
+  let getTheDot=word.substring(word.indexOf("."))
+
+  // push the dot to the array
+  results.push({word:getTheDot})
+} 
+//if the word include comma (,) 
+else if(word.includes(",")){
+ // get the word only without the comma like ( guess, => guess)
+ let getTheWordWithoutTheComma= word.substring(0 , word.indexOf(","))
+
+ //push the word 
+ results.push({word:getTheWordWithoutTheComma})
+
+
+ // get the comma without the the word like (guess, => ,)
+ let getTheComma=word.substring(word.indexOf(","))
+
+ // push the comma to the array
+ results.push({word:getTheComma})
+}
+}
+})
+
+console.log(results)
+
   return {}; // This line is currently wrong :)
+
 }
 
 /**
